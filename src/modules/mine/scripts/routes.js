0740c1094281ccfd78b16688886de0080bd06441
hyxkiller <546495041@qq.com>
2017-10-29 14:34:38 +0800

    我的模块合并

---
+++ src/modules/mine/scripts/routes.js
@@ -0,0 +1,29 @@
+import Mine from "../components/mine.vue";
+import Login from "../components/login.vue";
+import Register from '../components/register.vue';
+import Setting from '../components/setting.vue';
+
+export default [
+    {
+        path: '/Mine',
+        component: Mine,
+        name: 'Mine',
+        children: [
+            {
+                path: '/Mine/Setting',
+                component: Setting,
+                name: 'Setting'
+            }
+        ] 
+    },
+    {
+        path: '/Login',
+        component: Login,
+        name: 'Login'
+    },
+    {
+        path: '/Register',
+        component: Register,
+        name: 'Register'
+    }
+]
